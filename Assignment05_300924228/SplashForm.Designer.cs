d707ffc62401aa051125754521af9d72110f5ad0
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-08-10 00:50:49 -0400

    Created SplashFormTimer_Tick event handler

--- Assignment05_300924228/SplashForm.Designer.cs
+++ Assignment05_300924228/SplashForm.Designer.cs
@@ -29,10 +29,31 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.SplashFormTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // SplashFormTimer
+            // 
+            this.SplashFormTimer.Enabled = true;
+            this.SplashFormTimer.Interval = 3000;
+            this.SplashFormTimer.Tick += new System.EventHandler(this.SplashFormTimer_Tick);
+            // 
+            // SplashForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(304, 441);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimizeBox = false;
+            this.Name = "SplashForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer SplashFormTimer;
     }
 }
--- Assignment05_300924228/SplashForm.cs
+++ Assignment05_300924228/SplashForm.cs
@@ -12,7 +12,7 @@ using System.Windows.Forms;
  * Student No: 300924228
  * Date: August 10, 2017
  * Description: This is the SplashForm class for the Assignment 05 BMI Calculator
- * Version:0.1- Thi SplashForm will be displaced for 3 seconds and will be displaced by BMI Calculator
+ * Version:0.2- Created SplashFormTimer_Tick event handler
  */
 namespace Assignment05_300924228
 {
@@ -21,6 +21,19 @@ namespace Assignment05_300924228
         public SplashForm()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// This is the SplashFormTimer_Tick event handler
+        /// This event handler displays SplashForm form for 3 seconds and replaced by bmiCalculatorform
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SplashFormTimer_Tick(object sender, EventArgs e)
+        {
+            BMICalculatorForm bmiCalculatorForm = new BMICalculatorForm();
+            bmiCalculatorForm.Show();
+            this.Hide();
+            SplashFormTimer.Enabled = false; //triggers only once
         }
     }
 }
