2b33b84a6bf32ff7c8c7ad4f619670831f9c2e10
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-08-10 00:35:55 -0400

    Created SplashForm/Staarted SplashForm first in program class

--- Assignment05_300924228/Program.cs
+++ Assignment05_300924228/Program.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
  * Student No: 300924228
  * Date: August 10, 2017
  * Description: This is the BMI Calculator wondow form for the prospective Assignment 05 of COMP123-s2017
- * This calculator prompts users to input information like gender, age height and weight and returns a standard result
+ * Version: 0.1-Started SplashForm first
  */
 namespace Assignment05_300924228
 {
@@ -22,7 +22,7 @@ namespace Assignment05_300924228
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new BMICalculatorForm());
+            Application.Run(new SplashForm());
         }
     }
 }
---
+++ Assignment05_300924228/SplashForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Name: Bishnu Khanal
+ * Student No: 300924228
+ * Date: August 10, 2017
+ * Description: This is the SplashForm class for the Assignment 05 BMI Calculator
+ * Version:0.1- Thi SplashForm will be displaced for 3 seconds and will be displaced by BMI Calculator
+ */
+namespace Assignment05_300924228
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
